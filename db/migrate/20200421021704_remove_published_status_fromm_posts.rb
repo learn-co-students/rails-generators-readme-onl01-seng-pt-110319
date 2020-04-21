991ca26ebfe89d06a09fe9ded76a03d482f0e204
github username <github email address>
2020-04-20 22:57:45 -0400

    Done.

---
+++ db/migrate/20200421021704_remove_published_status_fromm_posts.rb
@@ -0,0 +1,4 @@
+class RemovePublishedStatusFrommPosts < ActiveRecord::Migration[5.0]
+  def change
+  end
+end
---
+++ db/migrate/20200421022305_change_post_status_data_ttype_to_posts.rb
@@ -0,0 +1,4 @@
+class ChangePostStatusDataTtypeToPosts < ActiveRecord::Migration[5.0]
+  def change
+  end
+end
